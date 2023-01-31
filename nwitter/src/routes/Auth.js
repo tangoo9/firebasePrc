77179f242acaeb8b6e679458ea19420e5c268e6b
김태우 <kokicola@naver.com>
2023-01-31 23:04:16 +0900

    login form

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+
+	const onChange = (e) =>{
+		const {target : {name, value}} = e;
+		if(name === 'email'){
+			setEmail(value)
+		}else if(name === 'password'){
+			setPassword(value)
+		}
+	}
+
+	const onSubmit = (e) =>{
+		e.preventDefault();
+		console.log(email, password)
+	}
+	return (
+		<>
+			<div>Auth</div>
+			<form onSubmit={onSubmit}>
+				<input name='email' type="text" placeholder='email' onChange={onChange} required></input>
+				<input name='password' type="password" placeholder='password' onChange={onChange} required></input>
+				<input type="submit" value='login'></input>
+			</form>
+			<button> continue with google</button>
+			<button> continue with github</button>
+		</>
+	)
 }
 
 export default Auth
