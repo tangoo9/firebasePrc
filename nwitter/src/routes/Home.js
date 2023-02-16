6869e9e126ee4aef04839e9acfeceb5345228ba0
김태우 <kokicola@naver.com>
2023-02-16 22:37:07 +0900

    이미지 업로드 , fileReader API (js)

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -8,11 +8,12 @@ const Home = ({userObj}) => {
 	
 	const [nweet, setNweet ] = useState('');
 	const [nweets, setNweets] = useState([]);
+	const [imageFile, setImageFile] = useState();
 
 	useEffect(()=>{
 		const q = query(
 			collection(dbService, "nweets"),
-			orderBy("createdAt", "desc")
+			orderBy("createdAt", "asc")
 			);
 			onSnapshot(q, (snapshot) => {
 			const nweetArray = snapshot.docs.map((doc) => ({
@@ -43,12 +44,37 @@ const Home = ({userObj}) => {
 		const {target : {value} } = e;
 		setNweet(value); 
 	}
+
+	const onFileChange = (e) =>{
+		// console.log(e.target.files)
+		const {target: {files}} =e;
+		const theFile = files[0];
+		// console.log(theFile)
+		const reader = new FileReader()
+		reader.onloadend = (finishedEvent) =>{
+			// console.log(finishedEvent)
+			const {currentTarget :{result}} = finishedEvent;
+			setImageFile(result)
+		}
+		reader.readAsDataURL(theFile);
+	}
 	
+	const onRemoveImage =()=>{
+		setImageFile(null)
+	}
+
 	return (
 		<>
 			<form onSubmit={onSubmit}>
 				<input type="text" onChange={onChange} placeholder="what's on your mind?" maxLength={120}></input>
+				<input type="file" accept='image/*' onChange={onFileChange}></input>
 				<input type="submit" value="Nweet"></input>
+				{imageFile &&   (
+					<>
+						<img src={imageFile} alt="none" width="50px" height="50px"/>
+						<button onClick={onRemoveImage}>선택 취소</button>
+					</>
+				)}
 			</form>
 			<div>
 				{nweets.map(nweet => (
